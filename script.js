efb41edd70df8a30a33a62d7ab4c870c543663d1
eviarmayani1975-creator <eviarmayani1975@gmail.com>
2026-07-04 22:43:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const tombol = document.getElementById("open");
 
+const cover = document.querySelector(".cover");
+
+const home = document.getElementById("home");
+
+tombol.onclick = function () {
+
+  cover.style.opacity = "0";
+  cover.style.transition = "0.8s";
+
+  setTimeout(() => {
+
+    cover.style.display = "none";
+    home.style.display = "block";
+
+  }, 800);
+
+}
